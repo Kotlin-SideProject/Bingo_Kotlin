02e378fba6779f531261e862a503d158fe7f54b6
pighaddt <pighaddt@gmail.com>
2020-05-12 11:36:20 +0800

    使用Firebase UI登入畫面功能

--- app/src/main/java/com/angus/bingo_kotlin/MainActivity.kt
+++ app/src/main/java/com/angus/bingo_kotlin/MainActivity.kt
@@ -2,11 +2,48 @@ package com.angus.bingo_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
 
+    companion object{
+        var TAG = MainActivity::class.java.simpleName
+        var RC_SIGN_IN = 100
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
+    }
+
+    override fun onAuthStateChanged(auth: FirebaseAuth) {
+        if(auth.currentUser ==  null){
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    AuthUI.IdpConfig.EmailBuilder().build(),
+                                    AuthUI.IdpConfig.GoogleBuilder().build()))
+                            .setIsSmartLockEnabled(false)
+                            .build()
+                    , RC_SIGN_IN)
+
+
+        }else{
+            Log.d(TAG, ": ${auth.currentUser!!.email}/${auth.currentUser!!.uid}");
+        }
     }
 }
