2b77f96e0059787c9ee281798abb495d3eb9729c
pighaddt <pighaddt@gmail.com>
2020-05-12 16:13:27 +0800

    Kotlin > 儲存資料到 Firebase 即時資料庫，run 擴充

--- app/src/main/java/com/angus/bingo_kotlin/MainActivity.kt
+++ app/src/main/java/com/angus/bingo_kotlin/MainActivity.kt
@@ -3,10 +3,13 @@ package com.angus.bingo_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.auth.api.Auth
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
 class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
@@ -31,19 +34,38 @@ class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
     }
 
     override fun onAuthStateChanged(auth: FirebaseAuth) {
-        if(auth.currentUser ==  null){
-            startActivityForResult(
-                    AuthUI.getInstance().createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    AuthUI.IdpConfig.EmailBuilder().build(),
-                                    AuthUI.IdpConfig.GoogleBuilder().build()))
-                            .setIsSmartLockEnabled(false)
-                            .build()
-                    , RC_SIGN_IN)
+        auth.currentUser?.also {
+            Log.d(TAG, ": ${it.email}/${it.uid}")
+            it.displayName?.run {
+                FirebaseDatabase.getInstance().getReference("users")
+                        .child(it.uid)
+                        .child("displayName")
+                        .setValue(this)
+                        .addOnCompleteListener { Log.d(TAG, ": done"); }
+            }
+        }?:signUp()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        }else{
-            Log.d(TAG, ": ${auth.currentUser!!.email}/${auth.currentUser!!.uid}");
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_menu_signout){
+            AuthUI.getInstance().signOut(this)
         }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun signUp() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                AuthUI.IdpConfig.EmailBuilder().build(),
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+//                        .setIsSmartLockEnabled(false)
+                        .build()
+                , RC_SIGN_IN)
     }
 }
