8f5b0d45aa7de91ddb29f7e46372babd1c8a31a9
pighaddt <pighaddt@gmail.com>
2020-06-04 10:36:10 +0800

    Kotlin > 選球功能與格狀 RecyclerView 進階設計

--- app/src/main/java/com/angus/bingo_kotlin/BingoActivity.kt
+++ app/src/main/java/com/angus/bingo_kotlin/BingoActivity.kt
@@ -2,18 +2,21 @@ package com.angus.bingo_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.common.ChangeEventType
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_bingo.*
 import kotlinx.android.synthetic.main.single_button.view.*
 
-class BingoActivity : AppCompatActivity() {
+class BingoActivity : AppCompatActivity(), View.OnClickListener {
     companion object{
         val TAG = BingoActivity::class.java.simpleName
         val STATUS_INIT = 0
@@ -54,6 +57,7 @@ class BingoActivity : AppCompatActivity() {
                 .child("status")
                 .setValue(STATUS_JOINED)
         }
+
         for (i in 0..24){
             val  button = NumberButton(this@BingoActivity)
             button.number = i+1
@@ -64,6 +68,10 @@ class BingoActivity : AppCompatActivity() {
         bingo_recycler.setHasFixedSize(true)
         bingo_recycler.layoutManager = GridLayoutManager(this@BingoActivity, 5)
 
+        val numberMap = mutableMapOf<Int, Int>()
+        for(i in 0..24){
+            numberMap.put(buttons.get(i).number, i)
+        }
         //Query  from FirebaseDatabase
         val query = FirebaseDatabase.getInstance().getReference("rooms")
             .child(roomId)
@@ -81,9 +89,32 @@ class BingoActivity : AppCompatActivity() {
 
             override fun onBindViewHolder(holder: numberHolder, position: Int, model: Boolean) {
                 holder.button.text = buttons[position].number.toString()
-                holder.button.isEnabled = !model
+//                holder.button.isEnabled = !model
+                holder.button.number = buttons.get(position).number
+                holder.button.isEnabled = !buttons.get(position).picked
+                holder.button.setOnClickListener(this@BingoActivity)
             }
-        }
+
+             override fun onChildChanged(
+                 type: ChangeEventType,
+                 snapshot: DataSnapshot,
+                 newIndex: Int,
+                 oldIndex: Int
+             ) {
+                 super.onChildChanged(type, snapshot, newIndex, oldIndex)
+                 if(type == ChangeEventType.CHANGED){
+                     val number = snapshot.key?.toInt()
+                     Log.d(TAG, "number: ${number}")
+                     val position = numberMap.get(number)
+                     Log.d(TAG, "position: ${position}");
+                     val picked = snapshot.value as Boolean
+                     Log.d(TAG, "picked:${picked} ");
+                     buttons.get(position!!).picked = picked
+                     val holder : numberHolder = bingo_recycler.findViewHolderForAdapterPosition(position!!) as numberHolder
+                     holder.button.isEnabled = !picked
+                 }
+             }
+         }
         bingo_recycler.adapter = adapter
 
     }
@@ -102,5 +133,14 @@ class BingoActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         adapter.stopListening()
+    }
+
+    override fun onClick(v: View?) {
+        val number = (v as NumberButton).number
+        FirebaseDatabase.getInstance().getReference("rooms")
+            .child(roomId)
+            .child("numbers")
+            .child(number.toString())
+            .setValue(true)
     }
 }
